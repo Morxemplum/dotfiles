f3d26ed894384ef75f756a5472707e203745bb02
Synth Morxemplum <morxemplum@gmail.com>
2024-10-17 16:58:17 -0700

    Refactor scripts

--- scripts/hypr-screenshot.sh
+++ scripts/hypr-screenshot.sh
@@ -1,6 +1,10 @@
+# Establish a destination and naming format
 IMG="$(xdg-user-dir PICTURES)/Screenshots/$(date +'Screenshot %Y-%m-%d %H-%M-%S')"
 
+# Use grim and slurp to capture a PNG of the image
 grim -g "$(slurp -d)" -l 9 "$IMG.png"
+# Convert to JPEG XL
 magick convert "$IMG.png" -quality 80 "$IMG.jxl"
+# Copy the PNG to clipboard (for compatibility reasons) and remove the png
 wl-copy < "$IMG.png"
 rm "$IMG.png"
--- scripts/waybar-reloadable.sh
+++ scripts/waybar-reloadable.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css"
+# Add any waybar files so they can be watched
+CONFIG_FILES="$HOME/dotfiles/config/waybar/config $HOME/dotfiles/config/waybar/style.css"
 
+# If this script stops, kill waybar
 trap "killall waybar" EXIT
 
+# Since waybar doesn't have hot reloading built in, this script watches for changes using inotify and reloads when any of our configs have been modified
 while true; do
     waybar &
     inotifywait -e create,modify $CONFIG_FILES
