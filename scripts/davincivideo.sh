9275f389b535136a2c0ba46e1fa2625025575834
Synth Morxemplum <morxemplum@gmail.com>
2024-11-05 16:14:08 -0700

    Refactor code in preparation for batch conversion

--- scripts/davincivideo.sh
+++ scripts/davincivideo.sh
@@ -15,20 +15,6 @@
 #              - Selectively encodes all audio tracks in a video file
 #
 # TODO: Support batch conversion
-# Note: I would use libopus and flac, but ffmpeg doesn't support containerizing
-#       them in mov. And DNxHR will not work in an MKV container.
-
-SHORT_ARGS="hop"
-LONG_ARGS=help,output,overwrite,proxy,studio,raw-audio
-PARSED=`getopt --options $SHORT_ARGS --longoptions $LONG_ARGS --name "$0" -- "$@"`
-
-# Program variables
-OUTPUT_DEST=""
-OVERWRITE=0 # 0 False; 1 True; 2 Ask
-PROXY=0
-PROXY_SCALE=100
-RAW_AUDIO=0
-STUDIO=0
 
 function list_help_info() {
   echo "Usage: davincivideo.sh [options] input_video"
@@ -54,6 +40,189 @@ function list_help_info() {
   echo "                           DaVinci Resolve has a harder time processing these codecs.)"
   echo ""
   echo "Requires ffprobe and ffmpeg for usage."
+}
+
+SHORT_ARGS="hop"
+LONG_ARGS=help,output,overwrite,proxy,studio,raw-audio
+PARSED=`getopt --options $SHORT_ARGS --longoptions $LONG_ARGS --name "$0" -- "$@"`
+
+## Program variables
+
+OUTPUT_DEST=""
+OVERWRITE=0 # 0 False; 1 True; 2 Ask
+PROXY=0
+PROXY_SCALE=100
+RAW_AUDIO=0
+STUDIO=0
+
+## FUNCTIONS
+
+# format_file_name(input_file_name)
+# Formats the input file name to match the file path and naming convention of the output file
+# Returns the output file name
+function format_file_name() {
+  # Strip the file extension
+  DESTINATION="${1%.*}"
+  # Strip the path to get the name
+  input_file=${DESTINATION##*/}
+
+  if [ "$OUTPUT_DEST" != "" ]; then
+    DESTINATION="$OUTPUT_DEST/$input_file"
+  fi
+
+  # Return the output file name (File extension will be added later)
+  output_file="$DESTINATION-T"
+  if (( PROXY == 1 )); then
+    output_file="${output_file}P"
+  fi
+}
+
+# video_codec_check(codec)
+# Takes the input codec and compares it against well-known codecs. A helper function to help the video transcode function on what to do
+# Returns with video_transcode and switch_container to their appropriate values
+function video_codec_check() {
+  # Is the video using a codec DV4L doesn't support?
+  if [[ ($1 == "h264" || $1 == "h265") && $STUDIO == "0" ]]; then
+    video_transcode=1
+    echo "    Video needs transcoding"
+  # Free codecs (like VP9 and AV1) are not compatible with MOV, so if we must preserve that codec, then we need to switch the container
+  elif [[ $1 == "vp9" || $1 == "av1" ]]; then
+    echo "    \"Free\" codec detected ($1). Switching QuickTime to Matroska"
+    # TODO: When adding a force transcode flag, recommend people to use this flag due to how demanding these codecs are
+    switch_container=1
+  else
+    echo "    Video is already in an acceptable codec"
+  fi
+}
+
+# setup_video_transcode(input_file_name)
+# Uses ffprobe to analyze the video and find out the video codec (and width and height if proxy is enabled). 
+# This function uses that information to determine if transcoding is needed and set the appropriate container.
+# Returns an ffmpeg string configured with the proper output codec (and scaled width and height if proxy is enable), along with the proper container
+function setup_video_transcode() {
+  # Use ffprobe and grab the necessary metadata
+  ffprobe -hide_banner -loglevel warning -i "$1" -print_format ini -show_format -select_streams v:0 -show_entries stream=$video_metadata -sexagesimal -o metadata.tmp.txt
+
+  video_codec=$(grep "codec_name" metadata.tmp.txt)
+  video_codec=${video_codec:11}
+
+  if [[ $PROXY == "1" && $PROXY_SCALE != "1" ]]; then
+    video_width=$(grep "width=" metadata.tmp.txt)
+    video_width=${video_width:6}
+    video_height=$(grep "height=" metadata.tmp.txt)
+    video_height=${video_height:7}
+
+    proxy_width=$(awk "BEGIN { print int($video_width * $PROXY_SCALE) }")
+    proxy_height=$(awk "BEGIN { print int($video_height * $PROXY_SCALE) }")
+
+    # echo "CODEC: $video_codec, RESOLUTION: $video_width x $video_height, PROXY RESOLUTION: $proxy_width x $proxy_height"
+  fi
+
+  video_transcode=0
+  switch_container=0
+  video_codec_check $video_codec
+
+  # TODO: Apply proxy scaling to videos that don't meet the criteria? Though this would involve having to learn various quirks of codecs.
+  if (( video_transcode == 1 )); then
+    quality_preset="dnxhr_hq"
+    if (( PROXY == 1 )); then
+      quality_preset="dnxhr_sq"
+    fi
+    vtranscode_str="-c:v dnxhd -profile:v $quality_preset -pix_fmt yuv422p"
+    if [[ $PROXY == "1" && $PROXY_SCALE != "1" ]]; then
+      echo "    Proxy Resolution: ${proxy_width}x${proxy_height}"
+      vtranscode_str="${vtranscode_str} -s ${proxy_width}x${proxy_height}"
+    fi
+  fi
+
+  if (( switch_container == 1 )); then
+    file_container="mkv"
+  fi
+}
+
+# find_existing_output(output_file)
+# Checks to see if the video has already been transcoded, and handles the behavior according to user preferences.
+# If the user specifies overwriting files, a string with the ffmpeg overwrite flag is returned
+function find_existing_output() {
+  if [ -f $1 ]; then
+    if (( OVERWRITE == 0 )); then
+      echo "    Transcoded output already exists. Skipping."
+      # TODO: When implementing batch conversion, change this to just end this conversion, instead of terminating the script
+      rm metadata.tmp.txt
+      exit 0
+    elif (( OVERWRITE == 1 )); then
+      echo "    Transcoded output already exists. Overwriting."
+      overwrite_str="-y"
+    fi
+    # As for 2, we don't need to do anything as ffmpeg will handle the prompt for us
+  fi
+}
+
+# audio_codec_check(codec, container)
+# Takes the input codec and compares it against well-known codecs. A helper function to help the audio transcode function on what to do
+# Returns a value to indicate whether the codec is supported by DV4L and needs transcoding.
+function audio_codec_check() {
+  # DV4L doesn't support AAC or Ogg Vorbis, regardless of Free or Studio.
+  # FFmpeg doesn't support Opus in mov.
+  if [[ ($1 == "aac" || $1 == "vorbis") || ($1 == "opus" && $2 == "mov") ]]; then
+    audio_transcode=1
+  fi
+}
+
+# transcode_audio_tracks(input_file_name, file_container)
+# Uses ffprobe and ffmpeg to analyze all audio tracks on a video and transcodes them to mp3 (or raw PCM)
+# This function separates transcoded audio tracks into their own files due to ffmpeg limitations, which need to be merged.
+# Returns two ffmpeg strings: One for inputting the audio files, and one for mapping them.
+# In addition, a boolean is returned if ANY audio track has been transcoded
+function transcode_audio_tracks() {
+  # Grab audio metadata
+  ffprobe -hide_banner -loglevel warning -i "$1" -print_format ini -show_format -select_streams a -show_entries stream=codec_name -sexagesimal -o metadata.tmp.txt
+
+  # Since there can be multiple audio tracks in a video file, separate all results into a list
+  audio_codecs=(`grep "codec_name" metadata.tmp.txt`)
+  i=0
+  tc=0 # boolean for tracking if current audio track was transcoded
+
+  for line in "${audio_codecs[@]}"; do
+    acodec="${line:11}"
+    audio_transcode=0
+    audio_codec_check $acodec $2
+
+    if (( audio_transcode == 1 )); then
+      echo "    Transcoding Audio Track #$i"
+      if (( RAW_AUDIO == 1 )); then
+        ffmpeg -y -hide_banner -loglevel warning -stats -i "$1" -map 0:a:$i -c:a pcm_s16le "a$i.tmp.wav"
+        input_str="${input_str} -i a$i.tmp.wav"
+      else
+        ffmpeg -y -hide_banner -loglevel warning -stats -i "$1" -map 0:a:$i -c:a libmp3lame "a$i.tmp.mp3"
+        input_str="${input_str} -i a$i.tmp.mp3"
+      fi
+      tc=1
+      tcr=1
+    else
+      echo "    Skipping Audio Track #$i"
+      map_str="${map_str} -map 0:a:$i"
+      tc=0
+    fi
+    i=$(( i+1 ))
+    if (( tc == 1 )); then
+      map_str="${map_str} -map $i"
+    fi
+  done
+}
+
+# clean_temp_files(clean_audio_files)
+# Cleans up all temporary files that were made by the script
+function clean_temp_files() {
+  echo "Cleaning temp files"
+  rm metadata.tmp.txt
+  if (( $1 == 1 )); then
+    if (( RAW_AUDIO == 1 )); then
+      rm a*.tmp.wav
+    else
+      rm a*.tmp.mp3
+    fi
+  fi
 }
 
 while true; do 
@@ -106,11 +275,9 @@ if [[ $# -eq 0 ]]; then
   exit 0
 fi
 
-# Strip the file extension
-DESTINATION="${1%.*}"
-# Strip the path to get the name
-input_file=${DESTINATION##*/}
-file_container="mov"
+# Pre-batch variables
+video_metadata="codec_name"
+tcr=0 # Boolean to check if ANY audio track was transcoded
 
 # Check if an output destination was specified, and if it exists.
 if [ "$OUTPUT_DEST" != "" ]; then
@@ -124,13 +291,8 @@ if [ "$OUTPUT_DEST" != "" ]; then
 		  exit 1
 	  fi
   fi
-  DESTINATION="$OUTPUT_DEST/$input_file"
 fi
 
-# Get the output file name (File extension will be added later)
-output_file="$DESTINATION-T"
-
-video_metadata="codec_name"
 if (( PROXY == 1 )); then
   # Sanity checks on the Proxy Scale
   if !(( PROXY_SCALE )); then
@@ -145,129 +307,32 @@ if (( PROXY == 1 )); then
   fi
   # Get the actual decimal point
   PROXY_SCALE=$(awk "BEGIN { print $PROXY_SCALE/100 }")
-  output_file="${output_file}P"
 fi
+
+# BEGIN VIDEO CONVERSION
 
 echo $1
 
+output_file=""
+format_file_name $1
+
 vtranscode_str="-c:v copy"
-video_transcode=0
-switch_container=0
-
-# Use ffprobe and grab the necessary metadata
-ffprobe -hide_banner -loglevel warning -i "$1" -print_format ini -show_format -select_streams v:0 -show_entries stream=$video_metadata -sexagesimal -o metadata.tmp.txt
-
-video_codec=$(grep "codec_name" metadata.tmp.txt)
-video_codec=${video_codec:11}
-
-if [[ $PROXY == "1" && $PROXY_SCALE != "1" ]]; then
-  video_width=$(grep "width=" metadata.tmp.txt)
-  video_width=${video_width:6}
-  video_height=$(grep "height=" metadata.tmp.txt)
-  video_height=${video_height:7}
-
-  proxy_width=$(awk "BEGIN { print int($video_width * $PROXY_SCALE) }")
-  proxy_height=$(awk "BEGIN { print int($video_height * $PROXY_SCALE) }")
-
-  # echo "CODEC: $video_codec, RESOLUTION: $video_width x $video_height, PROXY RESOLUTION: $proxy_width x $proxy_height"
-fi
-
-# Is the video using a codec DV4L doesn't support?
-if [[ ($video_codec == "h264" || $video_codec == "h265") && $STUDIO == "0" ]]; then
-  video_transcode=1
-  echo "    Video needs transcoding"
-# Free codecs (like VP9 and AV1) are not compatible with MOV, so if we must preserve that codec, then we need to switch the container
-elif [[ $video_codec == "vp9" || $video_codec == "av1" ]]; then
-  echo "    \"Free\" codec detected ($video_codec). Switching QuickTime to Matroska"
-  # TODO: When adding a force transcode flag, recommend people to use this flag due to how demanding these codecs are
-  switch_container=1
-else
-  echo "    Video is already in an acceptable codec"
-fi
-
-# TODO: Apply proxy scaling to videos that don't meet the criteria? Though this would involve having to learn various quirks of codecs.
-if (( video_transcode == 1 )); then
-  QUALITY_PRESET="dnxhr_hq"
-  if (( PROXY == 1 )); then
-    QUALITY_PRESET="dnxhr_sq"
-  fi
-  vtranscode_str="-c:v dnxhd -profile:v $QUALITY_PRESET -pix_fmt yuv422p"
-  if [[ $PROXY == "1" && $PROXY_SCALE != "1" ]]; then
-    echo "    Proxy Resolution: ${proxy_width}x${proxy_height}"
-    vtranscode_str="${vtranscode_str} -s ${proxy_width}x${proxy_height}"
-  fi
-fi
-
-if (( switch_container == 1 )); then
-  file_container="mkv"
-fi
+file_container="mov"
+setup_video_transcode $1
 
 output_file="${output_file}.$file_container"
-
-# Check if the transcoded video already exists
 overwrite_str=""
-if [ -f $output_file ]; then
-  if (( OVERWRITE == 0 )); then
-    echo "    Transcoded output already exists. Skipping."
-    # TODO: When implementing batch conversion, change this to just end this conversion, instead of terminating the script
-    rm metadata.tmp.txt
-    exit 0
-  elif (( OVERWRITE == 1 )); then
-    echo "    Transcoded output already exists. Overwriting."
-    overwrite_str="-y"
-  fi
-  # As for 2, we don't need to do anything as ffmpeg will handle the prompt for us
-fi
+find_existing_output $output_file
 
-# Grab audio metadata
-ffprobe -hide_banner -loglevel warning -i "$1" -print_format ini -show_format -select_streams a -show_entries stream=codec_name -sexagesimal -o metadata.tmp.txt
-
-# Since there can be multiple audio tracks in a video file, separate all results into a list
-audio_codecs=(`grep "codec_name" metadata.tmp.txt`)
-i=0
-tc=0 # boolean for tracking if current audio track was transcoded
-tcr=0 # Boolean to check if ANY audio track was transcoded
 input_str="-i $1"
 map_str="-map 0:v"
-
-# Due to ffmpeg limitations, we must split each transcoded audio track into their own file and merge them back
-for line in "${audio_codecs[@]}"; do
-  acodec="${line:11}"
-  # DV4L doesn't support AAC or Ogg Vorbis, regardless of Free or Studio.
-  # FFmpeg doesn't support Opus in mov.
-  if [[ ($acodec == "aac" || $acodec == "vorbis") || ($acodec == "opus" && $file_container == "mov") ]]; then
-    echo "    Transcoding Audio Track #$i"
-    if (( RAW_AUDIO == 1 )); then
-      ffmpeg -y -hide_banner -loglevel warning -stats -i "$1" -map 0:a:$i -c:a pcm_s16le "a$i.tmp.wav"
-      input_str="${input_str} -i a$i.tmp.wav"
-    else
-      ffmpeg -y -hide_banner -loglevel warning -stats -i "$1" -map 0:a:$i -c:a libmp3lame "a$i.tmp.mp3"
-      input_str="${input_str} -i a$i.tmp.mp3"
-    fi
-    tc=1
-    tcr=1
-  else
-    echo "    Skipping Audio Track #$i"
-    map_str="${map_str} -map 0:a:$i"
-    tc=0
-  fi
-  i=$(( i+1 ))
-  if (( tc == 1 )); then
-    map_str="${map_str} -map $i"
-  fi
-done
+transcode_audio_tracks $1 $file_container
 
 # Use ffmpeg to convert the MP4 file to MOV with the specified framerate
 ffmpeg $overwrite_str -hide_banner -loglevel warning -stats $input_str $vtranscode_str -c:a copy $map_str "$output_file"
 
 echo "Conversion completed. Output file: $output_file"
-echo "Cleaning temp files"
-# Clean up temp files
-rm metadata.tmp.txt
-if (( tcr == 1 )); then
-  if (( RAW_AUDIO == 1 )); then
-    rm a*.tmp.wav
-  else
-    rm a*.tmp.mp3
-  fi
-fi
+
+# END VIDEO CONVERSION 
+
+clean_temp_files $tcr
