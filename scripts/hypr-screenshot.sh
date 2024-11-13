5e07c66b5e17c6baf461f9d2279fa1e360ae5604
Synth Morxemplum <morxemplum@gmail.com>
2024-11-13 00:07:15 -0700

    Hypr-screenshot now creates screenshot folder if non-existent

--- scripts/hypr-screenshot.sh
+++ scripts/hypr-screenshot.sh
@@ -1,5 +1,16 @@
-# Establish a destination and naming format
-IMG="$(xdg-user-dir PICTURES)/Screenshots/$(date +'Screenshot %Y-%m-%d %H-%M-%S')"
+# Establish a destination
+SCREENSHOTS_FOLDER="$(xdg-user-dir PICTURES)/Screenshots"
+# Check if screenshots folder exists
+if [ ! -d "$SCREENSHOTS_FOLDER" ]; then
+  mkdir "$SCREENSHOTS_FOLDER"
+  if [ ! -d "$SCREENSHOTS_FOLDER" ]; then
+    echo "Failed to make screenshots folder. Insufficient permissions?"
+    exit 1
+  fi
+fi
+
+# Establish naming format
+IMG="$SCREENSHOTS_FOLDER/$(date +'Screenshot %Y-%m-%d %H-%M-%S')"
 
 # Use grim and slurp to capture a PNG of the image
 grim -g "$(slurp -d)" -l 9 "$IMG.png"
