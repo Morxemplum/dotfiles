d6718e3630cda2adea4ab347513986f6cd973b72
Synth Morxemplum <morxemplum@gmail.com>
2024-11-17 15:17:07 -0700

    Setup distrobox

--- nix/system-packages.nix
+++ nix/system-packages.nix
@@ -26,10 +26,23 @@
 		
 		# Compatibility tools
 		appimage-run
+		distrobox
 	];
 
   # Enable nix command and flakes for searching purposes
 	nix.extraOptions = ''
 		experimental-features = nix-command flakes 
 	'';
+
+	# Enable common container config files in /etc/containers
+	virtualisation.containers.enable = true;
+	virtualisation = {
+	  podman = {
+	    enable = true;
+	    # Create a `docker` alias for podman, to use it as a drop-in replacement
+	    dockerCompat = true;
+	    # Required for containers under podman-compose to be able to talk to each other.
+	    defaultNetwork.settings.dns_enabled = true;
+	  };
+	};
 }
