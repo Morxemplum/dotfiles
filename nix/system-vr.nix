d71c8e3aae64800bae3374e1eda3300d7b45a157
Synth Morxemplum <morxemplum@gmail.com>
2024-12-26 19:42:28 -0700

    Use unstable version of ALVR (completely broken)

--- nix/system-vr.nix
+++ nix/system-vr.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
+let
+  unstable = import <nixos-unstable> { 
+    config.allowUnfree = true;
+  };
+in
 {
   programs.alvr = {
     enable = true;
-    package = pkgs.alvr;
+    # TODO: Completely switch VR applications. SteamVR immediately crashes when my headset tries to connect.
+    package = unstable.alvr;
     openFirewall = true;
   };
 }
