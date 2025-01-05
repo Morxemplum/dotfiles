682d5787dbf5fdc62fa7dfbe90c994998d2624bc
Synth Morxemplum <morxemplum@gmail.com>
2025-01-05 01:14:46 -0700

    Switch from ALVR to WiVRn (much more reliable)

--- nix/system-vr.nix
+++ nix/system-vr.nix
@@ -1,15 +1,47 @@
 { config, pkgs, ... }:
 
-let
-  unstable = import <nixos-unstable> { 
-    config.allowUnfree = true;
-  };
-in
 {
-  programs.alvr = {
+  environment.systemPackages = with pkgs; [
+    avahi
+    # TODO: If we get a Nvidia driver 565.77.01 or later, remove this package
+    monado-vulkan-layers
+  ];
+  hardware.graphics.extraPackages = with pkgs; [
+    monado-vulkan-layers
+  ];
+
+  services.wivrn = {
     enable = true;
-    # TODO: Completely switch VR applications. SteamVR immediately crashes when my headset tries to connect.
-    package = unstable.alvr;
     openFirewall = true;
+
+    defaultRuntime = true;
+
+    monadoEnvironment = {
+      STEAMVR_LH_ENABLE = "1";
+      XRT_COMPOSITOR_COMPUTE = "1";
+      WMR_HANDTRACKING = "0"; # Even though Quest 2 supports hand tracking, not my biggest priority
+    };
+
+    # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
+    config = {
+      enable = true;
+      json = {
+        # 1.0x foveation scaling
+        scale = 1.0;
+        # 50 Mb/s
+        bitrate = 50000000;
+        encoders = [
+          {
+            encoder = "nvenc";
+            codec = "h264";
+            # 1.0 x 1.0 scaling
+            width = 1.0;
+            height = 1.0;
+            offset_x = 0.0;
+            offset_y = 0.0;
+          }
+        ];
+      };
+    };
   };
 }
