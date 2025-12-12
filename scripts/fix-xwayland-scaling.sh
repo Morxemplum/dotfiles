36e3fb3f95c0bce5a1543b30b47092d494bc5d9c
Morxemplum <morxemplum+github@gmail.com>
2025-12-11 17:39:46 -0700

    scripts: Add script to fix XWayland scaling When you disable scaling from the Wayland compositor, most compositors (except for KDE Plasma) will choose to simply not scale the XWayland applications and just render them at 1x scale.
    Some XWayland applications will work just fine and scale properly, but some others need you to scale for them. This script tweaks some XOrg environment variables that will help instruct those applications to scale at the proper scale.
    
    The scale values are hardcoded in this script, so if you want to change to your respective scale factor, you'll need to edit this script.

---
+++ scripts/fix-xwayland-scaling.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Name: fix-xwayland-scaling.sh
+# Author: Morxemplum
+# Description: This is a startup shell script that is used to help XWayland
+# applications scale themselves properly to my HiDPI monitors. This is useful
+# for window managers / desktop environments that don't properly support X
+# applications scaling properly.
+
+export GDK_SCALE=2
+export GDK_DPI_SCALE=1
+export QT_SCALE_FACTOR=2
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+# Fix the cursor size
+export XCURSOR_SIZE=36 # 24 * 1.5
+
+DESIRED_DPI="144" # 96 * 1.5
+echo "Xft.dpi: ${DESIRED_DPI}" | xrdb -merge
+
+if [ -r "${HOME}/.Xresources" ] && command -v xrdb >/dev/null 2>&1; then
+	xrdb -merge "${HOME}/.Xresources"
+fi
+
+xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2
